516bd9857792152c8978dacf0685cb83ea624ad5
Filip <siposlaw95@gmail.com>
2022-11-24 00:21:57 +0100

    friendFunction

--- friend-functions-classes/friendFunction.cpp
+++ friend-functions-classes/friendFunction.cpp
@@ -1,50 +1,68 @@
-#include <iostream>  
+#include <iostream>
 using namespace std;
-class Szef;          
-class Pracownik  
-{  
-    
-    public: 
-	string nazwa; 
-    int godziny_pracy;  
-	int stawka_godzinowa;
-    void setdata(string pNazwa,int pGodziny_pracy, int pStawka_godzinowa)  
-    {  
-        nazwa=pNazwa; 
-		godziny_pracy=pGodziny_pracy;
-		stawka_godzinowa=pStawka_godzinowa; 
-    }  
-    friend void Wynagrodzenie(Pracownik,Szef);         // friend function.  
-};  
-class Szef  
-{  
-  
-	
-    public:  
+class Szef;
+class Pracownik
+{
+    int godziny_pracy;
+    int stawka_godzinowa;
+
+public:
     string nazwa;
-    int godziny_pracy;  
-	int stawka_godzinowa;
-    void setdata(string pNazwa,int pGodziny_pracy, int pStawka_godzinowa)  
-    {  
-        nazwa=pNazwa; 
-		godziny_pracy=pGodziny_pracy;
-		stawka_godzinowa=pStawka_godzinowa; 
-    }  
-    friend void Wynagrodzenie(Pracownik,Szef);                    // friend function  
-};  
-void Wynagrodzenie(Pracownik pPracownik,Szef pSzef)  
-{  
-   
-    cout <<"Imie pracownika "<<pPracownik.nazwa<<endl<<" Wynagrodzenie pracownika "<< pPracownik.godziny_pracy*pPracownik.stawka_godzinowa << endl;  
-    cout <<"Imie Szefa "<< pSzef.nazwa<<endl<<" Wynagrodzenie szefa " << pSzef.godziny_pracy*pSzef.stawka_godzinowa << endl; 
-     
-}  
-   int main()  
-{  
-   Pracownik pPracownik;  
-   Szef pSzef;  
-   pPracownik.setdata("Dave",40,8);  
-   pSzef.setdata("Justin", 60, 200);  
-   Wynagrodzenie(pPracownik,pSzef);  
-    return 0;  
- }    
+
+    void setdata(string pNazwa, int pGodziny_pracy, int pStawka_godzinowa)
+    {
+        nazwa = pNazwa;
+        godziny_pracy = pGodziny_pracy;
+        stawka_godzinowa = pStawka_godzinowa;
+    }
+    int getGodziny()
+    {
+        return godziny_pracy;
+    };
+    int getStawka()
+    {
+        return stawka_godzinowa;
+    };
+    friend void Wynagrodzenie(Pracownik, Szef); // friend function.
+};
+class Szef
+{
+    int godziny_pracy;
+    int stawka_godzinowa;
+
+public:
+    string nazwa;
+
+    void setdata(string pNazwa, int pGodziny_pracy, int pStawka_godzinowa)
+    {
+        nazwa = pNazwa;
+        godziny_pracy = pGodziny_pracy;
+        stawka_godzinowa = pStawka_godzinowa;
+    }
+    int getGodziny()
+    {
+        return godziny_pracy;
+    };
+    int getStawka()
+    {
+        return stawka_godzinowa;
+    };
+    friend void Wynagrodzenie(Pracownik, Szef); // friend function
+};
+void Wynagrodzenie(Pracownik pPracownik, Szef pSzef)
+{
+
+    cout << "Imie pracownika " << pPracownik.nazwa << endl
+         << " Wynagrodzenie pracownika " << pPracownik.getGodziny() * pPracownik.getStawka() << endl;
+    cout << "Imie Szefa " << pSzef.nazwa << endl
+         << " Wynagrodzenie szefa " << pSzef.getGodziny() * pSzef.getStawka() << endl;
+}
+int main()
+{
+    Pracownik pPracownik;
+    Szef pSzef;
+    pPracownik.setdata("Dave", 40, 8);
+    pSzef.setdata("Justin", 60, 200);
+    Wynagrodzenie(pPracownik, pSzef);
+    return 0;
+}
